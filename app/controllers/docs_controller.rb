2d262219196ea601e9de269318ac5d686abbd83c
nafidurmus <nafidurmus07@gmail.com>
2018-05-19 15:40:05 +0300

    used simple_form & created docs view for new and _form page

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -7,9 +7,17 @@ class DocsController < ApplicationController
 	end
 
 	def new
+		@doc = Doc.new
 	end
 
 	def create
+		@doc = Doc.new(doc_params)
+
+		if @doc.save
+			redirect_to @doc
+		else
+			render 'new'
+		end
 	end
 
 	def edit
@@ -22,11 +30,12 @@ class DocsController < ApplicationController
 	end
 
 	private 
-	
+
 		def find_doc
 		end
 
 		def doc_params
+			params.require(:doc).permit(:title, :content)
 		end
 
 end
